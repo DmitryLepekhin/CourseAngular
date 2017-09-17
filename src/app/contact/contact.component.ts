2a46c63853164ba2fa465cdea87d0b24971ed6e6
Dmitry Lepekhin <dmitry.lepekhin@gmail.com>
2017-09-17 19:54:11 +0300

    RxJS Part3

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -12,6 +12,35 @@ export class ContactComponent implements OnInit {
   feedbackForm: FormGroup;
   feedback: Feedback;
   contactType = ContactType;
+  formErrors = {
+    'firstname': '',
+    'lastname': '',
+    'telnum': '',
+    'email': ''
+  };
+  /*
+  Transcript: This JavaScript object, as you see, contains four elements here. Now, whenever I do the form validation, recall that I already specified that I am going to create a new method called onValueChanged. Inside the onValueChanged, the way the code is written is that if any error is detected, a string containing the message corresponding to that error will be added into this JavaScript object. So this way, within my code, I can do the form validation. Now, in addition to this, I will now define a few validation messages. Now, this particular pattern of doing things I learned by reading the documentation on form validation in the angular.io website. So, they have a whole documentation on how to do form validation and a link to which is available in the additional resources of this chapter and also the previous chapters on Angular forms. So there, in that particular document, they prescribed this as a way of doing form validation. And I found that to be very intuitive, so I also adopted the same pattern for doing form validation in code. So along with here, I'm going to define the validation messages for all the possible fields here. So for the firstname field, I will define a few validation messages here. So I would say, required, and then say, First Name is required.
+   */
+  validationMessages = {
+    'firstname': {
+      'required':      'First Name is required.',
+      'minlength':     'First Name must be at least 2 characters long.',
+      'maxlength':     'FirstName cannot be more than 25 characters long.'
+    },
+    'lastname': {
+      'required':      'Last Name is required.',
+      'minlength':     'Last Name must be at least 2 characters long.',
+      'maxlength':     'Last Name cannot be more than 25 characters long.'
+    },
+    'telnum': {
+      'required':      'Tel. number is required.',
+      'pattern':       'Tel. number must contain only numbers.'
+    },
+    'email': {
+      'required':      'Email is required.',
+      'email':         'Email not in valid format.'
+    },
+  };
 
   constructor(private fb: FormBuilder) {
     this.createForm();
@@ -22,14 +51,32 @@ export class ContactComponent implements OnInit {
 
   private createForm() {
     this.feedbackForm = this.fb.group({
-      firstname: ['', Validators.required],
-      lastname: ['', Validators.required],
-      telnum: ['', Validators.required],
-      email: ['', Validators.required],
+      firstname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)]],
+      lastname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)]],
+      telnum: ['', [Validators.required, Validators.pattern]],
+      email: ['', [Validators.required, Validators.email]],
       agree: false,
       contacttype: 'None',
       message: ''
-  });
+    });
+    this.feedbackForm.valueChanges
+      .subscribe(data => this.onValueChanged(data));
+    this.onValueChanged(); // (re)set form validation messages
+  }
+
+  onValueChanged(data?: any) {
+    if (this.feedbackForm) return;
+    const form = this.feedbackForm;
+    for (const field in this.formErrors) {
+      this.formErrors[field] = '';
+      const control = form.get(field);
+      if (control && control.dirty && !control.valid) {
+        const messages = this.validationMessages[field];
+        for (const key in control.errors) {
+          this.formErrors[key] += messages[key] + ' ';
+        }
+      }
+    }
   }
 
   onSubmit() {
